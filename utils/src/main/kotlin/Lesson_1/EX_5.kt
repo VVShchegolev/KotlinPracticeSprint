12f0fd5e140dc807821d905da2f3d11540294277
YGD_SHIRO_VVS <youngsonik14@gmail.com>
2025-06-24 00:11:59 +1000

    Update EX_5.kt

--- utils/src/main/kotlin/Lesson_1/EX_5.kt
+++ utils/src/main/kotlin/Lesson_1/EX_5.kt
@@ -4,10 +4,10 @@ fun main() {
 
     val totalSeconds: Int = 6480
 
-    val hours: Int = totalSeconds / 3600
-    val remainingSecondAfterHours: Int = totalSeconds % 3600
-    val minutes: Int =  remainingSecondAfterHours / 60
-    val seconds: Int =  remainingSecondAfterHours % 60
+    val hours: Int = totalSeconds / SECOND_IN_HOUR
+    val remainingSecondAfterHours: Int = totalSeconds % SECOND_IN_HOUR
+    val minutes: Int =  remainingSecondAfterHours / SECONDS_IN_MINUTE
+    val seconds: Int =  remainingSecondAfterHours % SECONDS_IN_MINUTE
 
 //    Отформатируем время
     val formattedTime = String.format("%02d:%02d:%02d", hours, minutes, seconds)
@@ -15,3 +15,6 @@ fun main() {
     println("Время, проведённое Юрием Гагариным в космосе: $formattedTime")
 
 }
+
+const val SECOND_IN_HOUR = 3600
+const val SECONDS_IN_MINUTE = 60
