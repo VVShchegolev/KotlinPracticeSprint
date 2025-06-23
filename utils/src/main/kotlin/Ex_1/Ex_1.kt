98cd5343dd8cc33212633d96698edc25155c8434
VVShchegolev <youngsonik14@gmail.com>
2025-06-23 15:09:24 +1000

    KS-1-5 EX-5

---
+++ utils/src/main/kotlin/Ex_1/Ex_1.kt
@@ -0,0 +1,16 @@
+package Ex_1
+
+fun main() {
+
+    val student1: Byte = 3
+    val student2: Byte = 4
+    val student3: Byte = 3
+    val student4: Byte = 5
+
+    val sum = student1 + student2 + student3 + student4
+
+    val average = sum.toDouble() / 4
+
+    println("Средний бал по английскому: $average")
+
+}
