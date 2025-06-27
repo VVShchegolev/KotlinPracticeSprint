407cf9a2867cac34c32e68332156005008001d06
VVShchegolev <youngsonik14@gmail.com>
2025-06-27 12:53:46 +1000

    KS-5-5 задача 5

---
+++ utils/src/main/kotlin/Lesson_5_Ex_5/Lesson_5_Ex_5.kt
@@ -0,0 +1,29 @@
+package Lesson_5_Ex_5
+
+fun main() {
+
+    val winningNumbers = List(NUMBER_COUNT) { (0..MAX_NUMBER).random() }
+
+    println("Введите первое число (от 0 до $MAX_NUMBER):")
+    val userNumber1 = readln().toInt()
+    println("Введите второе число (от 0 до $MAX_NUMBER):")
+    val userNumber2 = readln().toInt()
+    println("Введите третье число (от 0 до $MAX_NUMBER):")
+    val userNumber3 = readln().toInt()
+
+    val userNumbers = listOf(userNumber1, userNumber2, userNumber3)
+
+    val matches = userNumbers.count { it in winningNumbers }
+
+    when (matches) {
+        3 -> println("Поздравляем! Вы угадали все числа и выиграли джекпот!")
+        2 -> println("Вы угадали два числа и получаете крупный приз!")
+        1 -> println("Вы угадали одно число и получаете утешительный приз!")
+        else -> println("Неудача! Вы не угадали ни одного числа.")
+    }
+
+    println("Выигрышные числа: ${winningNumbers.joinToString(", ")}")
+}
+
+const val MAX_NUMBER = 42
+const val NUMBER_COUNT = 3
